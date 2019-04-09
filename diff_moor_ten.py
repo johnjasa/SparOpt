Fixed some bugs causing errors in gradients for certain combinations of design vars.

diff_moor_ten.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import root, fsolve
+from scipy.optimize import root, fsolve, least_squares
 
 from openmdao.api import ImplicitComponent
 
@@ -57,14 +57,16 @@ class DiffMoorTen(ImplicitComponent):
 			return [-x[0] + 1. + 1. / (mu * 9.80665) * x[1] * np.arcsinh(l_eff_lw * mu * 9.80665 / t_hor_lw) + t_hor_lw / (mu * 9.80665) * 1. / np.sqrt(1. + ((l_eff_lw * mu * 9.80665) / t_hor_lw)**2.) * (mu * 9.80665 / t_hor_lw * x[0] - l_eff_lw * mu * 9.80665 / t_hor_lw**2. * x[1]) + x[1] * l_eff_lw / EA + t_hor_lw / EA * x[0],\
 			-mu * 9.80665 * l_eff_lw / EA * x[0] - 1. / (mu * 9.80665) * x[1] * (np.sqrt(1. + ((l_eff_lw * mu * 9.80665) / t_hor_lw)**2.) - 1.) - t_hor_lw / (mu * 9.80665) * 1. / np.sqrt(1. + ((l_eff_lw * mu * 9.80665) / t_hor_lw)**2.) * (l_eff_lw * mu * 9.80665 / t_hor_lw) * (mu * 9.80665 / t_hor_lw * x[0] - l_eff_lw * mu * 9.80665 / t_hor_lw**2. * x[1])]
 
-		sol_ww = fsolve(fun_ww, [1.0, 1.0e3], xtol=1e-5)
+		#sol_ww = fsolve(fun_ww, [1.0, 1.0e3], xtol=1e-5)
+		sol_ww = least_squares(fun_ww, [1.0, 1.0e3], bounds=(np.array([0.,0.]),np.array([np.inf,np.inf])), xtol=1e-6)
 
-		sol_lw = fsolve(fun_lw, [-1.0, -1.0e3], xtol=1e-5)
+		#sol_lw = fsolve(fun_lw, [-1.0, -1.0e3], xtol=1e-5)
+		sol_lw = least_squares(fun_lw, [-1.0, -1.0e3], bounds=(np.array([-np.inf,-np.inf]),np.array([0.,0.])), xtol=1e-6)
 
-		outputs['deff_length_ww_dx'] = sol_ww[0]
-		outputs['dmoor_tension_ww_dx'] = sol_ww[1]
-		outputs['deff_length_lw_dx'] = sol_lw[0]
-		outputs['dmoor_tension_lw_dx'] = sol_lw[1]
+		outputs['deff_length_ww_dx'] = sol_ww.x[0]
+		outputs['dmoor_tension_ww_dx'] = sol_ww.x[1]
+		outputs['deff_length_lw_dx'] = sol_lw.x[0]
+		outputs['dmoor_tension_lw_dx'] = sol_lw.x[1]
 
 	def linearize(self, inputs, outputs, partials):
 		EA = inputs['EA_moor']

moor_ten_zero.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import root
+from scipy.optimize import root, least_squares
 
 from openmdao.api import ImplicitComponent
 
@@ -44,7 +44,8 @@ class MoorTenZero(ImplicitComponent):
 			t_star = x[1] / (mu * 9.80665)
 			return [l_tot - x[0] - l_tot_hor + t_star * np.arcsinh(x[0] / t_star) + x[1] * x[0] / EA, h - mu * 9.80665 * x[0]**2. / (2. * EA) - t_star * (np.sqrt(1. + (x[0] / t_star)**2.) - 1.)]
 
-		sol = root(fun, [600., 1.0e6])
+		#sol = root(fun, [600., 1.0e6])
+		sol = least_squares(fun, [600.0, 1.0e6], bounds=(np.array([0.,0.]),np.array([np.inf,np.inf])), xtol=1e-6)
 
 		outputs['eff_length_zero'] = sol.x[0]
 		outputs['moor_tension_zero'] = sol.x[1]

mooring_offset.py
@@ -60,7 +60,7 @@ class MooringOffset(ImplicitComponent):
 
 		#sol = root(fun, [600.0, 1.0e6, 600.0, 1.0e6, 25.], method='krylov', tol=1e-5) #TODO: zero tension and eff. length as inital guess?
 		#sol = fsolve(fun, [600.0, 1.0e6, 500.0, 5.0e5, 5.], xtol=1e-5)
-		sol = least_squares(fun, [600.0, 1.0e6, 500.0, 5.0e5, 5.], bounds=(np.array([0.,0.,0.,0.,0.]),np.array([np.inf,np.inf,np.inf,np.inf,300.])), xtol=1e-5)
+		sol = least_squares(fun, [600.0, 1.0e6, 500.0, 5.0e5, 5.], bounds=(np.array([0.,0.,0.,0.,0.]),np.array([np.inf,np.inf,np.inf,np.inf,300.])), xtol=1e-6)
 
 		outputs['eff_length_offset_ww'] = sol.x[0]
 		outputs['moor_tension_offset_ww'] = sol.x[1]

partials_check.py
@@ -4,30 +4,30 @@ import matplotlib.pyplot as plt
 from openmdao.api import Problem, IndepVarComp, DirectSolver, NewtonSolver, KSComp
 from openmdao.utils.visualization import partial_deriv_plot
 
-from dyn_tower_drag import DynTowerDrag
+from max_mooring_offset import MaxMooringOffset
 
 freqs = {\
-'omega' : np.linspace(0.014361566416410483,6.283185307179586,70), \
-'omega_wave': np.linspace(0.12,6.28,50)}
+'omega' : np.linspace(0.001,4.5,70), \
+'omega_wave': np.linspace(0.12,4.5,50)}
 
 EC = {\
 'N_EC' : 1}
 
 prob = Problem()
 ivc = IndepVarComp()
-ivc.add_output('D_tower', val=np.random.rand(10)*1e2)
-ivc.add_output('Z_tower', val=np.linspace(-120.,10.,11))
-ivc.add_output('L_tower', val=np.random.rand(10)*1e2)
-ivc.add_output('windspeed_0', val=50.)
-ivc.add_output('Cd_tower', val=0.7)
-ivc.add_output('CoG_rotor', val=119.)
-ivc.add_output('rho_wind', val=1.25)
+ivc.add_output('z_moor', val=-77.2)
+ivc.add_output('water_depth', val=320.)
+ivc.add_output('EA_moor', val=729000000.)
+ivc.add_output('mass_dens_moor', val=154.869)
+ivc.add_output('len_hor_moor', val=838.67)
+ivc.add_output('len_tot_moor', val=902.2)
 
 prob.model.add_subsystem('prob_vars', ivc, promotes=['*'])
 
-comp = prob.model.add_subsystem('check', DynTowerDrag(), promotes_inputs=['D_tower', 'Z_tower', 'L_tower', 'windspeed_0', 'Cd_tower', 'CoG_rotor', 'rho_wind'], promotes_outputs=['Fdyn_tower_drag', 'Mdyn_tower_drag'])
+comp = prob.model.add_subsystem('check', MaxMooringOffset(), promotes_inputs=['z_moor', 'water_depth', 'EA_moor', 'mass_dens_moor', 'len_hor_moor', 'len_tot_moor'], promotes_outputs=['moor_tension_max_offset_ww', 'eff_length_max_offset_ww', 'maxval_fairlead'])
 
 prob.setup(force_alloc_complex=True)
+prob.run_model()
 comp.set_check_partial_options(wrt='*', step=1e-8, method='cs')
 check_partials_data = prob.check_partials(show_only_incorrect=True)
 

run_spar_reduced.py
@@ -166,7 +166,7 @@ from openmdao.api import pyOptSparseDriver
 #driver = prob.driver = ScipyOptimizeDriver()
 driver = prob.driver = pyOptSparseDriver()
 driver.options['optimizer'] = 'SNOPT'
-driver.opt_settings['Major feasibility tolerance'] = 1e-3
+driver.opt_settings['Major feasibility tolerance'] = 1e-5
 driver.opt_settings['Major optimality tolerance'] = 1e-3
 
 driver.recording_options['includes'] = []
@@ -180,7 +180,7 @@ driver.add_recorder(recorder)
 #prob.model.add_design_var('D_spar_cp', lower=5.*np.ones(5), upper=20.*np.ones(5))
 #prob.model.add_design_var('L_spar_cp', lower=np.array([3., 3., 3., 10.]), upper=30.*np.ones(4))
 prob.model.add_design_var('D_spar_p', lower=5.*np.ones(11), upper=20.*np.ones(11))
-prob.model.add_design_var('L_spar', lower=np.array([3., 3., 3., 3., 3., 3., 3., 3., 3., 10.]), upper=30.*np.ones(10))
+prob.model.add_design_var('L_spar', lower=np.array([3., 3., 3., 3., 3., 3., 3., 3., 3., 11.]), upper=30.*np.ones(10))
 prob.model.add_design_var('z_moor', lower=-320., upper=-10.)
 
 prob.model.add_design_var('D_tower_cp', lower=5.*np.ones(4), upper=20.*np.ones(4))

viscous_damping.py
@@ -27,7 +27,7 @@ class ViscousDamping(ExplicitComponent):
 		D_spar = inputs['D_spar']
 		z_sparnode = inputs['z_sparnode']
 		x_sparelem = inputs['x_sparelem']
-		stddev_vel_distr = inputs['stddev_vel_distr'] #np.array([0.17856366, 0.14913746, 0.13331558, 0.12660471, 0.12420269, 0.12422336, 0.13459537, 0.15813834, 0.18982264, 0.22625248, 0.25732357, 0.257976, 0.2964546])
+		stddev_vel_distr = np.array([0.17856366, 0.14913746, 0.13331558, 0.12660471, 0.12420269, 0.12422336, 0.13459537, 0.15813834, 0.18982264, 0.22625248, 0.25732357, 0.257976, 0.2964546])
 
 		N_elem = len(x_sparelem)
 
@@ -63,7 +63,7 @@ class ViscousDamping(ExplicitComponent):
 		D_spar = inputs['D_spar']
 		z_sparnode = inputs['z_sparnode']
 		x_sparelem = inputs['x_sparelem']
-		stddev_vel_distr = inputs['stddev_vel_distr'] #np.array([0.17856366, 0.14913746, 0.13331558, 0.12660471, 0.12420269, 0.12422336, 0.13459537, 0.15813834, 0.18982264, 0.22625248, 0.25732357, 0.257976, 0.2964546])
+		stddev_vel_distr = np.array([0.17856366, 0.14913746, 0.13331558, 0.12660471, 0.12420269, 0.12422336, 0.13459537, 0.15813834, 0.18982264, 0.22625248, 0.25732357, 0.257976, 0.2964546])
 
 
 		N_elem = len(x_sparelem)
@@ -121,37 +121,37 @@ class ViscousDamping(ExplicitComponent):
 				partials['B_visc_11', 'z_sparnode'][0,i] += -0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D
 				partials['B_visc_11', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D
 				partials['B_visc_11', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * dz
-				partials['B_visc_11', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * dz
+				#partials['B_visc_11', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * dz
 
 				partials['B_visc_15', 'Cd'] += 0.5 * 1025. * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * z * dz
 				partials['B_visc_15', 'z_sparnode'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * (-z + dz * 0.5)
 				partials['B_visc_15', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * (z + dz * 0.5 )
 				partials['B_visc_15', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * z * dz
-				partials['B_visc_15', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z * dz
+				#partials['B_visc_15', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z * dz
 
 				partials['B_visc_17', 'Cd'] += 0.5 * 1025. * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i] * dz
 				partials['B_visc_17', 'z_sparnode'][0,i] += -0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i]
 				partials['B_visc_17', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i]
 				partials['B_visc_17', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * x_sparelem[i] * dz
-				partials['B_visc_17', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * x_sparelem[i] * dz
+				#partials['B_visc_17', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * x_sparelem[i] * dz
 				partials['B_visc_17', 'x_sparelem'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * dz
 
 				partials['B_visc_55', 'Cd'] += 0.5 * 1025. * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * z**2. * dz
 				partials['B_visc_55', 'z_sparnode'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * (-z**2. + z * dz)
 				partials['B_visc_55', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * (z**2. + z * dz)
 				partials['B_visc_55', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * z**2. * dz
-				partials['B_visc_55', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z**2. * dz
+				#partials['B_visc_55', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z**2. * dz
 
 				partials['B_visc_57', 'Cd'] += 0.5 * 1025. * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * z * x_sparelem[i] * dz
 				partials['B_visc_57', 'z_sparnode'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i] * (-z + 0.5 * dz)
 				partials['B_visc_57', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i] * (z + 0.5 * dz)
 				partials['B_visc_57', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * z * x_sparelem[i] * dz
-				partials['B_visc_57', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z * x_sparelem[i] * dz
+				#partials['B_visc_57', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * z * x_sparelem[i] * dz
 				partials['B_visc_57', 'x_sparelem'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * z * dz
 
 				partials['B_visc_77', 'Cd'] += 0.5 * 1025. * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i]**2. * dz
 				partials['B_visc_77', 'z_sparnode'][0,i] += -0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i]**2.
 				partials['B_visc_77', 'z_sparnode'][0,i+1] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i]**2.
 				partials['B_visc_77', 'D_spar'][0,j] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * x_sparelem[i]**2. * dz
-				partials['B_visc_77', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * x_sparelem[i]**2. * dz
+				#partials['B_visc_77', 'stddev_vel_distr'][0,i] += 0.5 * 1025. * Cd * np.sqrt(8./np.pi) * D * x_sparelem[i]**2. * dz
 				partials['B_visc_77', 'x_sparelem'][0,i] += 1025. * Cd * np.sqrt(8./np.pi) * stddev_vel_distr[i] * D * x_sparelem[i] * dz